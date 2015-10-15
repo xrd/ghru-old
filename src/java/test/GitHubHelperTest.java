ce9ff47762dc1d7a6188bb658d50b4d4c33783bd
Chris Dawson <xrdawson@gmail.com>
2015-10-14 20:00:06 -0700

    Bad first try

---
+++ src/java/test/GitHubHelperTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+import org.junit.Test;
+//import java.util.regex.Pattern;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+
+public class GitHubHelperTest {
+
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        // assertThat(EmailValidator.isValidEmail("name@email.com"), is(true));
+    }
+
+}
